1e3d87886ebd3ffb0515bcc1ea09efc62bb4dd7d
daunito sp41n <dmitrysp41n@gmail.com>
2018-12-22 21:29:46 +0200

    Added new project for role names

---
+++ PhotoAlbum.Constans/RoleName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Constans
+{
+    public static class RoleName
+    {
+        public const string Admin = "Administrators";
+        public const string User = "Users";
+    }
+}
