e685e55498962bbe40def40d9b6a855bfa283234
Dmitry Shevchenko <dmitrysp41n@gmail.com>
2019-01-09 00:00:24 +0200

    Temporary data removed

--- PhotoAlbum.DAL/EF/PhotoAlbumContext.cs
+++ PhotoAlbum.DAL/EF/PhotoAlbumContext.cs
@@ -30,41 +30,6 @@ namespace PhotoAlbum.DAL.EF
         public virtual DbSet<Photo> Photos { get; set; }
         public virtual DbSet<ClientProfile> ClientProfiles { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<ApplicationUser>()
-            //    .HasKey(c => c.Id)
-            //    .Property(x => x.ClientProfileId);
-
-            //modelBuilder.Entity<ApplicationUser>()
-            //    .HasKey(c=>c.Id)
-            //    .HasRequired(c => c.ClientProfile);
-
-            //modelBuilder.Entity<ClientProfile>()
-            //    .HasKey(c=>c.Id)
-            //    .HasRequired(c => c.ApplicationUser);
-
-            //.WithRequiredPrincipal(c => c.ApplicationUser);
-            //modelBuilder.Entity<ClientProfile>()
-            //    .HasOptional(x => x.ApplicationUser);
-
-            //modelBuilder.Entity<ClientProfile>()
-            //    .HasKey(x => x.Id)
-            //    .Property(x => x.Id)
-            //    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-
-            //modelBuilder.Entity<ClientProfile>().HasRequired(x => x.ApplicationUser).WithRequiredPrincipal(x => x.ClientProfile);
-
-
-
-
-
-
-
-
-
-            base.OnModelCreating(modelBuilder);
-        }
     }
 
 }
--- PhotoAlbum.DAL/EF/PhotoAlbumInitializer.cs
+++ PhotoAlbum.DAL/EF/PhotoAlbumInitializer.cs
@@ -13,7 +13,7 @@ namespace PhotoAlbum.DAL.EF
 {
     internal class PhotoAlbumInitializer : DropCreateDatabaseIfModelChanges<PhotoAlbumContext>
     {
-        private void Method(PhotoAlbumContext context)
+        protected override void Seed(PhotoAlbumContext context)
         {
             AppUserManager userManager = new AppUserManager(new UserStore<ApplicationUser, ApplicationRole, int, CustomUserLogin, CustomUserRole, CustomUserClaim>(context));
             AppRoleManager roleManager = new AppRoleManager(new CustomRoleStore(context));
@@ -54,130 +54,6 @@ namespace PhotoAlbum.DAL.EF
                 userManager.AddToRole(user.Id, RoleName.User);
 
             context.SaveChanges();
-        }
-
-        protected override void Seed(PhotoAlbumContext context)
-        {
-            Method(context);
-
-
-
-
-
-
-            //var photo = new Photo()
-            //{
-            //    Description = "test photo",
-            //    LastUpdate = DateTime.Now,
-            //    UploadedDate = DateTime.Now
-            //};
-
-            //var photo2 = new Photo()
-            //{
-            //    Description = "test photo 2",
-            //    LastUpdate = DateTime.Now,
-            //    UploadedDate = DateTime.Now
-            //};
-
-            //var photo3 = new Photo()
-            //{
-            //    Description = "test photo 3",
-            //    LastUpdate = DateTime.Now,
-            //    UploadedDate = DateTime.Now
-            //};
-
-            //var photo4 = new Photo()
-            //{
-            //    Description = "test photo 4",
-            //    LastUpdate = DateTime.Now,
-            //    UploadedDate = DateTime.Now
-            //};
-
-            //var photo5 = new Photo()
-            //{
-            //    Description = "test photo 5",
-            //    LastUpdate = DateTime.Now,
-            //    UploadedDate = DateTime.Now
-            //};
-
-            //var gallery = new Gallery()
-            //{
-            //    Photos = new List<Photo>()
-            //    {
-            //        photo, photo2, photo3, photo4, photo5
-            //    }
-            //};
-
-            ////photo.GalleryId = gallery.Id;
-            ////photo2.GalleryId = gallery.Id;
-            ////photo3.GalleryId = gallery.Id;
-            ////photo4.GalleryId = gallery.Id;
-            ////photo5.GalleryId = gallery.Id;
-            
-
-            //var role = new Role() { };
-            //var role2 = new Role() {};
-            //var role3 = new Role() {  };
-            //var role4 = new Role() { };
-            //var role5 = new Role() {  };
-
-            //var user = new User()
-            //{
-            //    UserName = "Dmitry",
-            //    DateOfBirdth = new DateTime(1998, 11, 8),
-            //    Description = "Im programmer",
-            //    Email = "dmitrysp41n@gmail.com",
-            //    //Password = "password123",
-            //    //GalleryId = gallery.Id
-            //};
-
-            //var user2 = new User()
-            //{
-            //    UserName = "Alex",
-            //    DateOfBirdth = new DateTime(1991, 2, 8),
-            //    Description = "Im Alex",
-            //    Email = "Alex@gmail.com",
-            //    //Password = "AlexPassword",
-            //    //GalleryId = gallery.Id
-            //};
-
-            //var user3 = new User()
-            //{
-            //    UserName = "Max",
-            //    DateOfBirdth = new DateTime(1998, 10, 1),
-            //    Description = "Im Max",
-            //    Email = "Max@gmail.com",
-            //    //Password = "MaxPass",
-            //    //GalleryId = gallery.Id
-            //};
-
-            //var user4 = new User()
-            //{
-            //    UserName = "Moisiyaha",
-            //    DateOfBirdth = new DateTime(1998, 11, 14),
-            //    Description = "Im HS",
-            //    Email = "HS@gmail.com",
-            //    //Password = "passwordHS",
-            //    //GalleryId = gallery.Id
-            //};
-
-            //var user5 = new User()
-            //{
-            //    UserName = "Nikita",
-            //    DateOfBirdth = new DateTime(1998, 4, 6),
-            //    Description = "Im programmer",
-            //    Email = "Nikita@gmail.com",
-            //    //Password = "NikitaPass",
-            //    //GalleryId = gallery.Id
-            //};
-
-            //context.Photos.AddRange(new [] { photo, photo2, photo3, photo4, photo5 });
-            ////context.Users.AddRange(new [] { user, user2, user3, user4, user5 });
-            ////context.Roles.AddRange(new [] { role, role2, role3, role4, role5 });
-            //context.Galleries.Add(gallery);
-
-            //context.SaveChanges();
-            //base.Seed(context);
         }
     }
 }
